c156373372a45be7317d0450572fab4f85546b2b
Lin1ey <nguyen.numbers@gmail.com>
2024-04-30 21:00:55 -0400

    updated data and created types and interfaces for sinner identities

---
+++ src/types/data.ts
@@ -0,0 +1,42 @@
+export type Sin = "Wrath" | "Lust" | "Sloth" | "Gluttony" | "Gloom" | "Pride" | "Envy" | "None";
+export type DamageType = "Slash" | "Blunt" | "Pierce" | "None";
+export type SkillType = "Attack" | "Guard" | "Evade" | "Counter";
+export type PassiveType = "Owned" | "Res";
+
+export interface SkillDesc {
+  coin: number;
+  details: String[];
+}
+
+export interface Skill {
+  name: String;
+  affinity: Sin;
+  count: number;
+  base: number;
+  plus: number;
+  coins: number;
+  skillType: SkillType;
+  damageType: DamageType;
+  desc: SkillDesc[];
+  atkWeight: number;
+}
+
+export interface PassiveSkill {
+  name: string;
+  affinity: Sin;
+  cost: number;
+  passiveType: PassiveType;
+  desc: string[];
+}
+
+export interface Identity {
+  sinnerID: number;
+  name: string;
+  rarity: 1 | 2 | 3;
+  passive: PassiveSkill;
+  support: PassiveSkill;
+  skills: Skill[];
+  statusKeywords: string[];
+  sinKeywords: string[];
+  damageKeywords: string[];
+}
