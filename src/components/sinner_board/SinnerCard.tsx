b42b8b84e730dc8d959e0a30732e3b0248bda43a
Lin1ey <nguyen.numbers@gmail.com>
2024-05-06 23:34:02 -0400

    Sinner card  component

---
+++ src/components/sinner_board/SinnerCard.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Identity, SinnerSelectedState } from "../../types/data";
+import "@css/sinner_board/SinnerCard.css";
+import { getSinnerFromID, getSinnerImgPath } from "@helpers/parseData";
+
+type PrivateProps = {
+  id: Identity;
+  Content: React.ComponentType;
+};
+
+export type SinnerCardContentProps = {
+  cardState: SinnerSelectedState;
+  setSelectedState: Function;
+};
+
+function SinnerCard({ id, Content }: PrivateProps) {
+  const [sinnerId, setSinnerId] = useState(id.sinnerID);
+  const [name, setName] = useState(id.name);
+  const [selectedState, setSelectedState] = useState<SinnerSelectedState>("Support");
+  const [imagePath, setImagePath] = useState(getSinnerImgPath(getSinnerFromID(sinnerId), name));
+
+
+  return (
+    <div className="sinner-card">
+      <img className={`sinner-card-img ${selectedState}`} src={`${imagePath}`}></img>
+      <div className={`sinner-card-content`}>
+        <Content cardState={selectedState} setSelectedState={setSelectedState} />
+        <div className="sinner-card-name"><b>{name}</b></div>
+      </div>
+    </div>
+  );
+}
+
+export default SinnerCard;
